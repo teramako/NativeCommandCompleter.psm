7b932492ee38a9fea6b406c2a40ddd3b1b28efb4
teramako <teramako@gmail.com>
2025-10-22 01:23:00 +0900

    Register completer in root module

--- NativeCommandCompleter.psm1
+++ NativeCommandCompleter.psm1
@@ -2,3 +2,13 @@
  #  Root Module
  #>
 
+ if ([string]::IsNullOrEmpty($env:PS_COMPLETE_PATH))
+ {
+     $env:PS_COMPLETE_PATH = Join-Path $PSScriptRoot completions
+ }
+
+ Register-ArgumentCompleter -NativeFallback -ScriptBlock {
+     param($wordToComplete, $commandAst, $cursorPosition)
+     [MT.Comp.NativeCompleter]::Complete($wordToComplete, $commandAst, $cursorPosition)
+ }
+
