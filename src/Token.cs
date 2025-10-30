a371edd7bc27b554ca420ef6039e481322bc61e1
teramako <teramako@gmail.com>
2025-10-30 21:13:04 +0900

    Expose to public Token.Ast

--- src/Token.cs
+++ src/Token.cs
@@ -6,20 +6,20 @@ public class Token
 {
     public Token(CommandElementAst ast)
     {
-        _ast = ast;
+        Ast = ast;
         Position = ast.Extent.Text.Length;
     }
     public Token(CommandElementAst ast, int cursorPosition)
     {
-        _ast = ast;
+        Ast = ast;
         Position = cursorPosition - ast.Extent.StartOffset;
         IsTarget = true;
     }
 
-    private CommandElementAst _ast;
+    public CommandElementAst Ast { get; }
     public bool IsTarget { get; }
     public int Position { get; set; }
-    public string Value => _ast.Extent.Text;
+    public string Value => Ast.Extent.Text;
 
     public string Prefix => Position > 0 ? Value[0..Position] : Value;
     public string Suffix => Position > 0 ? Value[Position..] : string.Empty;
