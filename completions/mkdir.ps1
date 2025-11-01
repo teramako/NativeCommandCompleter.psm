a7ed0589e23b3a86b255d1a6671375e1315f0c74
teramako <teramako@gmail.com>
2025-11-02 07:59:50 +0900

    [completions] Fix `--mode` parameter for mkdir

--- completions/mkdir.ps1
+++ completions/mkdir.ps1
@@ -4,10 +4,7 @@
 Import-Module NativeCommandCompleter.psm
 
 Register-NativeCompleter -Name mkdir -Parameters @(
-    New-ParamCompleter -ShortName m -LongName mode -Description 'Set file mode (as in chmod)' -ArgumentCompleter {
-        param([string] $paramName, [string] $paramValue, [int] $position)
-        # TBD
-    }
+    New-ParamCompleter -ShortName m -LongName mode -Description 'Set file mode (as in chmod)' -Type Required
     New-ParamCompleter -ShortName p -LongName parents -Description 'Make parent directories as needed';
     New-ParamCompleter -ShortName v -LongName verbose -Description 'Print a message for each created directory';
     New-ParamCompleter -LongName version -Description 'Output version';
