0e2cfbcc6d07c2f78f69e212e39cec9d6fa05cee
teramako <teramako@gmail.com>
2025-11-02 07:56:35 +0900

    [completions] Fix `--reference` parameter for chmod

--- completions/chmod.ps1
+++ completions/chmod.ps1
@@ -1,8 +1,6 @@
 <#
  # chmod completion
  #>
-using namespace System.Management.Automation;
-
 Import-Module NativeCommandCompleter.psm
 
 Register-NativeCompleter -Name chmod -Parameters @(
@@ -11,10 +9,7 @@ Register-NativeCompleter -Name chmod -Parameters @(
     New-ParamCompleter -LongName preserve-root -Description  'Suppress most errors'
     New-ParamCompleter -ShortName f -LongName slient, quiet -Description 'Print a message for each created directory';
     New-ParamCompleter -ShortName v -LongName verbose -Description 'Prints each file processed'
-    New-ParamCompleter -LongName reference -Description 'Use RFILEs mode instead of MODE values' -ArgumentCompleter {
-        param([string] $paramName, [string] $paramValue, [int] $position)
-        [CompletionCompleters]::CompleteFilename($paramValue);
-    }
+    New-ParamCompleter -LongName reference -Description 'Use RFILEs mode instead of MODE values' -Type File
     New-ParamCompleter -ShortName R -LongName recursive -Description 'Operate recursively';
     New-ParamCompleter -LongName help -Description 'Display help and exit'
     New-ParamCompleter -LongName version -Description 'Display version and exit';
