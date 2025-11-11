0060342449e31bc015669e8c03c20c857d4238bc
teramako <teramako@gmail.com>
2025-11-12 01:13:26 +0900

    [completions] mkdir: supports localization

--- completions/mkdir.ps1
+++ completions/mkdir.ps1
@@ -3,11 +3,20 @@
  #>
 Import-Module NativeCommandCompleter.psm
 
-Register-NativeCompleter -Name mkdir -Parameters @(
-    New-ParamCompleter -ShortName m -LongName mode -Description 'Set file mode (as in chmod)' -Type Required
-    New-ParamCompleter -ShortName p -LongName parents -Description 'Make parent directories as needed';
-    New-ParamCompleter -ShortName v -LongName verbose -Description 'Print a message for each created directory';
-    New-ParamCompleter -LongName version -Description 'Output version';
-    New-ParamCompleter -LongName help -Description 'Display help'
-)
+$msg = data { ConvertFrom-StringData @'
+    mode    = Set file mode (as in chmod)
+    parents = Make parent directories as needed
+    verbose = Print a message for each created directory
+    version = Output version
+    help    = Display help
+'@ }
+Import-LocalizedData -BindingVariable localizedMessages -ErrorAction SilentlyContinue;
+foreach ($key in $localeMessages.Keys) { $msg[$key] = $localizedMessages[$key] }
 
+Register-NativeCompleter -Name mkdir -Parameters @(
+    New-ParamCompleter -ShortName m -LongName mode -Description $msg.mode -Type Required
+    New-ParamCompleter -ShortName p -LongName parents -Description $msg.parents
+    New-ParamCompleter -ShortName v -LongName verbose -Description $msg.verbose
+    New-ParamCompleter -LongName version -Description $msg.version
+    New-ParamCompleter -LongName help -Description $msg.help
+)
