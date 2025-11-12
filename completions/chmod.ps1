c8f077f531680e750f0c76dc850c1bc9061c7aef
teramako <teramako@gmail.com>
2025-11-12 12:17:13 +0900

    [completions] chmod: supports localization and update argument completer

--- completions/chmod.ps1
+++ completions/chmod.ps1
@@ -3,19 +3,33 @@
  #>
 Import-Module NativeCommandCompleter.psm -ErrorAction SilentlyContinue
 
+$msg = data { ConvertFrom-StringData @'
+    changes          = Like -v but report only changes
+    no-preserve-root = Don't treat / special (default)
+    preserve-root    = Suppress most errors
+    silent           = Print a message for each created directory
+    verbose          = Prints each file processed
+    reference        = Use RFILEs mode instead of MODE values
+    recursive        = Operate recursively
+    help             = Display help and exit
+    version          = Display version and exit
+'@ }
+Import-LocalizedData -BindingVariable localizedMessages -ErrorAction SilentlyContinue;
+foreach ($key in $localeMessages.Keys) { $msg[$key] = $localizedMessages[$key] }
+
 Register-NativeCompleter -Name chmod -Parameters @(
-    New-ParamCompleter -ShortName c -LongName changes -Description 'Like -v but report only changes'
-    New-ParamCompleter -LongName no-preserve-root -Description "Don't treat / special (default)"
-    New-ParamCompleter -LongName preserve-root -Description  'Suppress most errors'
-    New-ParamCompleter -ShortName f -LongName slient, quiet -Description 'Print a message for each created directory';
-    New-ParamCompleter -ShortName v -LongName verbose -Description 'Prints each file processed'
-    New-ParamCompleter -LongName reference -Description 'Use RFILEs mode instead of MODE values' -Type File
-    New-ParamCompleter -ShortName R -LongName recursive -Description 'Operate recursively';
-    New-ParamCompleter -LongName help -Description 'Display help and exit'
-    New-ParamCompleter -LongName version -Description 'Display version and exit';
+    New-ParamCompleter -ShortName c -LongName changes -Description $msg."changes"
+    New-ParamCompleter -LongName no-preserve-root -Description $msg."no-preserve-root"
+    New-ParamCompleter -LongName preserve-root -Description $msg."preserve-root"
+    New-ParamCompleter -ShortName f -LongName silent, quiet -Description $msg."slient"
+    New-ParamCompleter -ShortName v -LongName verbose -Description $msg."verbose"
+    New-ParamCompleter -LongName reference -Type File -Description $msg."reference"
+    New-ParamCompleter -ShortName R -LongName recursive -Description $msg."recursive"
+    New-ParamCompleter -LongName help -Description $msg."help"
+    New-ParamCompleter -LongName version -Description $msg."version"
 ) -ArgumentCompleter {
-    param([string] $wordToComplete, [int] $position, [int] $argIndex, [MT.Comp.CompletionContext] $context)
-    if ($argIndex -eq 0 -and -not $context.BoundParameters.ContainsKey("reference"))
+    param([int] $position, [int] $argIndex)
+    if ($argIndex -eq 0 -and -not $this.BoundParameters.ContainsKey("reference"))
     {
         return $null
     }
