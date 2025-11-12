3c5383b2ae4dc14c983b86da0ddfe2e011030cc9
teramako <teramako@gmail.com>
2025-11-12 12:26:00 +0900

    [completions] rmdir: supports localization

--- completions/rmdir.ps1
+++ completions/rmdir.ps1
@@ -3,10 +3,20 @@
  #>
 Import-Module NativeCommandCompleter.psm -ErrorAction SilentlyContinue
 
+$msg = data { ConvertFrom-StringData @'
+    ignore-fail-on-non-empty = Ignore errors from non-empty directories
+    parents                  = Remove each component of path
+    verbose                  = Verbose mode
+    help                     = Display help and exit
+    version                  = Display version and exit
+'@ }
+Import-LocalizedData -BindingVariable localizedMessages -ErrorAction SilentlyContinue;
+foreach ($key in $localeMessages.Keys) { $msg[$key] = $localizedMessages[$key] }
+
 Register-NativeCompleter -Name rmdir -Parameters @(
-    New-ParamCompleter -LongName ignore-fail-on-non-empty -Description 'Ignore errors from non-empty directories'
-    New-ParamCompleter -ShortName p -LongName parents -Description 'Remove each component of path'
-    New-ParamCompleter -ShortName v -LongName verbose -Description 'Verbose mode'
-    New-ParamCompleter -LongName help -Description 'Display help and exit'
-    New-ParamCompleter -LongName version -Description 'Display version and exit';
+    New-ParamCompleter -LongName ignore-fail-on-non-empty -Description $msg."ignore-fail-on-non-empty"
+    New-ParamCompleter -ShortName p -LongName parents -Description $msg."parents"
+    New-ParamCompleter -ShortName v -LongName verbose -Description $msg."verbose"
+    New-ParamCompleter -LongName help -Description $msg."help"
+    New-ParamCompleter -LongName version -Description $msg."version"
 )
