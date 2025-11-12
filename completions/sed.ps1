926a73fa6ab822fb93db02166276fb3dbddb8f26
teramako <teramako@gmail.com>
2025-11-12 11:44:00 +0900

    [completions] `sed`: supports localization and update argument completer

--- completions/sed.ps1
+++ completions/sed.ps1
@@ -3,29 +3,49 @@
  #>
 Import-Module NativeCommandCompleter.psm -ErrorAction SilentlyContinue
 
+$msg = data { ConvertFrom-StringData @'
+    quiet     = Silent mode
+    debug     = Annotate program execution
+    expression = Evaluate expression
+    file       = Evaluate file
+    follow-symlinks = Follow symlinks when processing in place
+    in-place        = Edit files in place
+    line-length     = Specify line-length
+    posix           = Disable all GNU extensions
+    regexp-extended = Use extended regexp
+    separate        = Consider files as separate
+    sandbox         = Operate in sandbox mode (disable e/r/w commands).
+    unbuffered      = Use minimal IO buffers
+    null-data       = Separate lines by NUL characters
+    help            = Display help and exit
+    version         = Display version and exit
+'@ }
+Import-LocalizedData -BindingVariable localizedMessages -ErrorAction SilentlyContinue;
+foreach ($key in $localeMessages.Keys) { $msg[$key] = $localizedMessages[$key] }
+
 if ($IsLinux)
 {
     Register-NativeCompleter -Name sed -Description 'stream editor' -Parameters @(
-        New-ParamCompleter -ShortName n -LongName quiet, slient -Description 'Silent mode'
-        New-ParamCompleter -LongName debug -Description 'Annotate program execution'
-        New-ParamCompleter -ShortName e -LongName expression -Description 'Evaluate expression' -Type Required
-        New-ParamCompleter -ShortName f -LongName file -Description 'Evaluate file' -Type File
-        New-ParamCompleter -LongName follow-symlinks -Description 'Follow symlinks when processing in place'
-        New-ParamCompleter -ShortName i -LongName in-place -Description 'Edit files in place' -Type FlagOrValue
-        New-ParamCompleter -ShortName l -LongName line-length -Description "Specify line-length" -Type Required
-        New-ParamCompleter -LongName posix -Description 'Disable all GNU extensions'
-        New-ParamCompleter -ShortName E,r -LongName regexp-extended -Description 'Use extended regexp'
-        New-ParamCompleter -ShortName s -LongName separate -Description 'Consider files as separate'
-        New-ParamCompleter -LongName sandbox -Description 'Operate in sandbox mode (disable e/r/w commands).'
-        New-ParamCompleter -ShortName u -LongName unbuffered -Description 'Use minimal IO buffers'
-        New-ParamCompleter -ShortName z -LongName null-data -Description 'Separate lines by NUL characters'
-        New-ParamCompleter -LongName help -Description 'Display help and exit'
-        New-ParamCompleter -LongName version -Description 'Display version and exit'
+        New-ParamCompleter -ShortName n -LongName quiet, slient -Description $msg."quiet"
+        New-ParamCompleter -LongName debug -Description $msg."debug"
+        New-ParamCompleter -ShortName e -LongName expression -Type Required -Description $msg."expression"
+        New-ParamCompleter -ShortName f -LongName file -Type File -Description $msg."file"
+        New-ParamCompleter -LongName follow-symlinks -Description $msg."follow-symlinks"
+        New-ParamCompleter -ShortName i -LongName in-place -Type FlagOrValue -Description $msg."in-place"
+        New-ParamCompleter -ShortName l -LongName line-length -Type Required -Description $msg."line-length"
+        New-ParamCompleter -LongName posix -Description $msg."posix"
+        New-ParamCompleter -ShortName E,r -LongName regexp-extended -Description $msg."regexp-extended"
+        New-ParamCompleter -ShortName s -LongName separate -Description $msg."separate"
+        New-ParamCompleter -LongName sandbox -Description $msg."sandbox"
+        New-ParamCompleter -ShortName u -LongName unbuffered -Description $msg."unbuffered"
+        New-ParamCompleter -ShortName z -LongName null-data -Description $msg."null-data"
+        New-ParamCompleter -LongName help -Description $msg."help"
+        New-ParamCompleter -LongName version -Description $msg."version"
     ) -ArgumentCompleter {
-        param([string] $wordToComplete, [int] $position, [int] $argIndex, [MT.Comp.CompletionContext] $context)
-        if ($argIndex -eq 0 -and -not $context.BoundParameters.ContainsKey("expression"))
+        param([int] $position, [int] $argIndex)
+        if ($argIndex -eq 0 -and -not $this.BoundParameters.ContainsKey("expression"))
         {
-            if ([string]::IsNullOrEmpty($wordToComplete))
+            if ([string]::IsNullOrEmpty($_))
             {
                 "pattern`tSpecify a pattern"
             }
