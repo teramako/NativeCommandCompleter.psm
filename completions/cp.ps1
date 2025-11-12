190ec32e9ff7b031231a788e0642822b588a1d3f
teramako <teramako@gmail.com>
2025-11-12 13:47:59 +0900

    [completions] cp: supports localization and determinates GNU

--- completions/cp.ps1
+++ completions/cp.ps1
@@ -1,53 +1,130 @@
 <#
  # cp completion
  #>
-Import-Module NativeCommandCompleter.psm
 Import-Module NativeCommandCompleter.psm -ErrorAction SilentlyContinue
 
-if ($IsLinux)
+$msg = data { ConvertFrom-StringData @'
+    gnu.archive                = Same as -dpR
+    gnu.attributes-only        = Copy just the attributes
+    gnu.backup                 = Make backup of each existing destination file
+    gnu.backup.none            = Never make backups
+    gnu.backup.numbered        = Make numbered backups
+    gnu.backup.existing        = Numbered backups if any exist, else simple
+    gnu.backup.simple          = Make simple backups
+    gnu.short_backup           = Make backup of each existing destination file
+    gnu.copy-contents          = Copy contents of special files when recursive
+    gnu.short_d                = Same as --no-dereference --preserve=link
+    gnu.force                  = Do not prompt before overwriting
+    gnu.interactive            = Prompt before overwrite
+    gnu.short_H                = Follow command-line symbolic links
+    gnu.link                   = Link files instead of copying
+    gnu.strip-trailing-slashes = Remove trailing slashes from source
+    gnu.suffix                 = Backup suffix
+    gnu.target-directory       = Target directory
+    gnu.update                 = Do not overwrite newer files
+    gnu.verbose                = Verbose mode
+    gnu.help                   = Display help and exit
+    gnu.version                = Display version and exit
+    gnu.dereference            = Always follow symbolic links
+    gnu.no-clobber             = Do not overwrite an existing file
+    gnu.no-dereference         = Never follow symbolic links
+    gnu.short_p                = Same as --preserve=mode,ownership,timestamps
+    gnu.preserve               = Preserve ATTRIBUTES if possible
+    gnu.no-preserve            = Don't preserve ATTRIBUTES
+    gnu.parents                = Use full source file name under DIRECTORY
+    gnu.recursive              = Copy directories recursively
+    gnu.reflink                = Control clone/CoW copies
+    gnu.remove-destination     = First remove existing destination files
+    gnu.sparse                 = Control creation of sparse files
+    gnu.symbolic-link          = Make symbolic links instead of copying
+    gnu.no-target-directory    = Treat DEST as a normal file
+    gnu.one-file-system        = Stay on this file system
+    gnu.short_Z                = Set SELinux context of copy to default type
+    gnu.context                = Set SELinux context of copy to CONTEXT
+    macos.recursive            = Copy directories recursively
+    macos.follow-symlink       = -R: Follow symlink arguments
+    macos.follow-all-symlink   = -R: Follow all symlinks
+    macos.dont-follow-symlink  = -R: Don't follow symlinks (default)
+    macos.force                = Don't confirm to overwrite
+    macos.interactive          = Prompt before overwrite
+    macos.dont-overwrite       = Don't overwrite existing
+    macos.preserve             = Preserve attributes of source
+    macos.verbose              = Print filenames as they're copied
+    macos.archive              = Archive mode (-pPR)
+    macos.clonefile2           = Clone using clonefile(2)
+    macos.ommit-xattrs         = Omit xattrs, resource forks
+    macos.hardlink             = Hard link instead of copying
+    macos.dont-traverse        = Don't traverse mount points
+    macos.symlink              = Symlink instead of copying
+'@ }
+Import-LocalizedData -BindingVariable localizedMessages -ErrorAction SilentlyContinue;
+foreach ($key in $localeMessages.Keys) { $msg[$key] = $localizedMessages[$key] }
+
+cp --version 2>/dev/null >/dev/null # GNU cp
+if ($LASTEXITCODE -eq 0)
 {
     Register-NativeCompleter -Name cp -Parameters @(
-        New-ParamCompleter -ShortName a -LongName archive -Description "Same as -dpR"
-        New-ParamCompleter -LongName attributes-only -Description "Copy just the attributes"
-        New-ParamCompleter -LongName backup -Description "Make backup of each existing destination file" -Arguments @(
-            "none `tNever make backups"
-            "off `tNever make backups"
-            "numbered `tMake numbered backups"
-            "t `tMake numbered backups"
-            "existing `tNumbered backups if any exist, else simple"
-            "nil `tNumbered backups if any exist, else simple"
-            "simple `tMake simple backups"
-            "never `tMake simple backups"
+        New-ParamCompleter -ShortName a -LongName archive -Description $msg."gnu.archive"
+        New-ParamCompleter -LongName attributes-only -Description $msg."gnu.attributes-only"
+        New-ParamCompleter -LongName backup -Description $msg."gnu.backup" -Arguments @(
+            "none `t{0}" -f $msg."gnu.backup.none"
+            "off `t{0}" -f $msg."gnu.backup.none"
+            "numbered `t{0}" -f $msg."gnu.backup.numbered"
+            "t `t{0}" -f $msg."gnu.backup.numbered"
+            "existing `t{0}" -f $msg."gnu.backup.existing"
+            "nil `t{0}" -f $msg."gnu.backup.existing"
+            "simple `t{0}" -f $msg."gnu.backup.simple"
+            "never `t{0}" -f $msg."gnu.backup.simple"
         )
-        New-ParamCompleter -ShortName b -Description "Make backup of each existing destination file"
-        New-ParamCompleter -LongName copy-contents -Description "Copy contents of special files when recursive"
-        New-ParamCompleter -ShortName d -Description "Same as --no-dereference --preserve=link"
-        New-ParamCompleter -ShortName f -LongName force -Description "Do not prompt before overwriting"
-        New-ParamCompleter -ShortName i -LongName interactive -Description "Prompt before overwrite"
-        New-ParamCompleter -ShortName H -Description "Follow command-line symbolic links"
-        New-ParamCompleter -ShortName l -LongName link -Description "Link files instead of copying"
-        New-ParamCompleter -LongName strip-trailing-ShortNamelashes -Description "Remove trailing slashes from source"
-        New-ParamCompleter -ShortName S -LongName suffix -Description "Backup suffix" -Type Required
-        New-ParamCompleter -ShortName t -LongName target-directory -Description "Target directory" -Type Directory
-        New-ParamCompleter -ShortName u -LongName update -Description "Do not overwrite newer files"
-        New-ParamCompleter -ShortName v -LongName verbose -Description "Verbose mode"
-        New-ParamCompleter -LongName help -Description "Display help and exit"
-        New-ParamCompleter -LongName version -Description "Display version and exit"
-        New-ParamCompleter -ShortName L -LongName dereference -Description "Always follow symbolic links"
-        New-ParamCompleter -ShortName n -LongName no-clobber -Description "Do not overwrite an existing file"
-        New-ParamCompleter -ShortName P -LongName no-dereference -Description "Never follow symbolic links"
-        New-ParamCompleter -ShortName p -Description "Same as --preserve=mode,ownership,timestamps"
-        New-ParamCompleter -LongName preserve -Description "Preserve ATTRIBUTES if possible" -Arguments 'mode','ownership','timestamps','links','all'
-        New-ParamCompleter -LongName no-preserve -Description "Don't preserve ATTRIBUTES" -Arguments 'mode','ownership','timestamps','links','all'
-        New-ParamCompleter -LongName parents -Description "Use full source file name under DIRECTORY"
-        New-ParamCompleter -ShortName r,R -LongName recursive -Description "Copy directories recursively"
-        New-ParamCompleter -LongName reflink -Description "Control clone/CoW copies" -Type FlagOrValue -Arguments 'always','auto','never'
-        New-ParamCompleter -LongName remove-destination -Description "First remove existing destination files"
-        New-ParamCompleter -LongName sparse -Description "Control creation of sparse files" -Arguments 'always','auto','never'
-        New-ParamCompleter -ShortName s -LongName symbolic-link -Description "Make symbolic links instead of copying"
-        New-ParamCompleter -ShortName T -LongName no-target-directory -Description "Treat DEST as a normal file"
-        New-ParamCompleter -ShortName x -LongName one-file-ShortNameystem -Description "Stay on this file system"
-        New-ParamCompleter -ShortName Z -Description "Set SELinux context of copy to default type"
-        New-ParamCompleter -ShortName X -LongName context -Description "Set SELinux context of copy to CONTEXT" -Type Required
+        New-ParamCompleter -ShortName b -Description $msg."gnu.short_backup"
+        New-ParamCompleter -LongName copy-contents -Description $msg."gnu.copy-contents"
+        New-ParamCompleter -ShortName d -Description $msg."gnu.short_d"
+        New-ParamCompleter -ShortName f -LongName force -Description $msg."gnu.force"
+        New-ParamCompleter -ShortName i -LongName interactive -Description $msg."gnu.interactive"
+        New-ParamCompleter -ShortName H -Description $msg."gnu.short_H"
+        New-ParamCompleter -ShortName l -LongName link -Description $msg."gnu.link"
+        New-ParamCompleter -LongName strip-trailing-slashes -Description $msg."gnu.strip-trailing-slashes"
+        New-ParamCompleter -ShortName S -LongName suffix -Description $msg."gnu.suffix" -Type Required
+        New-ParamCompleter -ShortName t -LongName target-directory -Description $msg."gnu.target-directory" -Type Directory
+        New-ParamCompleter -ShortName u -LongName update -Description $msg."gnu.update"
+        New-ParamCompleter -ShortName v -LongName verbose -Description $msg."gnu.verbose"
+        New-ParamCompleter -LongName help -Description $msg."gnu.help"
+        New-ParamCompleter -LongName version -Description $msg."gnu.version"
+        New-ParamCompleter -ShortName L -LongName dereference -Description $msg."gnu.dereference"
+        New-ParamCompleter -ShortName n -LongName no-clobber -Description $msg."gnu.no-clobber"
+        New-ParamCompleter -ShortName P -LongName no-dereference -Description $msg."gnu.no-dereference"
+        New-ParamCompleter -ShortName p -Description $msg."gnu.short_p"
+        New-ParamCompleter -LongName preserve -Description $msg."gnu.preserve" -Arguments 'mode','ownership','timestamps','links','all'
+        New-ParamCompleter -LongName no-preserve -Description $msg."gnu.no-preserve" -Arguments 'mode','ownership','timestamps','links','all'
+        New-ParamCompleter -LongName parents -Description $msg."gnu.parents"
+        New-ParamCompleter -ShortName r,R -LongName recursive -Description $msg."gnu.recursive"
+        New-ParamCompleter -LongName reflink -Description $msg."gnu.reflink" -Type FlagOrValue -Arguments 'always','auto','never'
+        New-ParamCompleter -LongName remove-destination -Description $msg."gnu.remove-destination"
+        New-ParamCompleter -LongName sparse -Description $msg."gnu.sparse" -Arguments 'always','auto','never'
+        New-ParamCompleter -ShortName s -LongName symbolic-link -Description $msg."gnu.symbolic-link"
+        New-ParamCompleter -ShortName T -LongName no-target-directory -Description $msg."gnu.no-target-directory"
+        New-ParamCompleter -ShortName x -LongName one-file-system -Description $msg."gnu.one-file-system"
+        New-ParamCompleter -ShortName Z -Description $msg."gnu.short_Z"
+        New-ParamCompleter -ShortName X -LongName context -Description $msg."gnu.context" -Type Required
+    )
+}
+else
+{
+    Register-NativeCompleter -Name cp -Parameters @(
+        New-ParamCompleter -ShortName R -Description $msg."macos.recursive"
+        New-ParamCompleter -ShortName H -Description $msg."macos.follow-symlink"
+        New-ParamCompleter -ShortName L -Description $msg."macos.follow-all-symlink"
+        New-ParamCompleter -ShortName P -Description $msg."macos.dont-follow-symlink"
+        New-ParamCompleter -ShortName f -Description $msg."macos.force"
+        New-ParamCompleter -ShortName i -Description $msg."macos.interactive"
+        New-ParamCompleter -ShortName n -Description $msg."macos.dont-overwrite"
+        New-ParamCompleter -ShortName p -Description $msg."macos.preserve"
+        New-ParamCompleter -ShortName v -Description $msg."macos.verbose"
+        New-ParamCompleter -ShortName a -Description $msg."macos.archive"
+        New-ParamCompleter -ShortName c -Description $msg."macos.clonefile2"
+        New-ParamCompleter -ShortName X -Description $msg."macos.ommit-xattrs"
+        New-ParamCompleter -ShortName l -Description $msg."macos.hardlink"
+        New-ParamCompleter -ShortName x -Description $msg."macos.dont-traverse"
+        New-ParamCompleter -ShortName s -Description $msg."macos.symlink"
     )
 }
