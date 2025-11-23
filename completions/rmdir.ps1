bf151742805089950c7bae214b3bad5b1e094e17
teramako <teramako@gmail.com>
2025-11-23 23:14:53 +0900

    [completions] Add rmdir

---
+++ completions/rmdir.ps1
@@ -0,0 +1,12 @@
+<#
+ # rmdir completion
+ #>
+Import-Module NativeCommandCompleter.psm
+
+Register-NativeCompleter -Name rmdir -Parameters @(
+    New-ParamCompleter -LongName ignore-fail-on-non-empty -Description 'Ignore errors from non-empty directories'
+    New-ParamCompleter -ShortName p -LongName parents -Description 'Remove each component of path'
+    New-ParamCompleter -ShortName v -LongName verbose -Description 'Verbose mode'
+    New-ParamCompleter -LongName help -Description 'Display help and exit'
+    New-ParamCompleter -LongName version -Description 'Display version and exit';
+)
