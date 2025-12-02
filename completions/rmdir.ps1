8cf669d86bf65232a13535e5bd57646cce2e5965
teramako <teramako@gmail.com>
2025-12-02 20:42:28 +0900

    [completions] 📝 Change key names

--- completions/rmdir.ps1
+++ completions/rmdir.ps1
@@ -4,7 +4,7 @@
 Import-Module NativeCommandCompleter.psm -ErrorAction SilentlyContinue
 
 $msg = data { ConvertFrom-StringData @'
-    ignore-fail-on-non-empty = Ignore errors from non-empty directories
+    ignoreFailOnNonEmpty     = Ignore errors from non-empty directories
     parents                  = Remove each component of path
     verbose                  = Verbose mode
     help                     = Display help and exit
@@ -14,9 +14,9 @@ Import-LocalizedData -BindingVariable localizedMessages -ErrorAction SilentlyCon
 foreach ($key in $localizedMessages.Keys) { $msg[$key] = $localizedMessages[$key] }
 
 Register-NativeCompleter -Name rmdir -Parameters @(
-    New-ParamCompleter -LongName ignore-fail-on-non-empty -Description $msg."ignore-fail-on-non-empty"
-    New-ParamCompleter -ShortName p -LongName parents -Description $msg."parents"
-    New-ParamCompleter -ShortName v -LongName verbose -Description $msg."verbose"
-    New-ParamCompleter -LongName help -Description $msg."help"
-    New-ParamCompleter -LongName version -Description $msg."version"
+    New-ParamCompleter -LongName ignore-fail-on-non-empty -Description $msg.ignoreFailOnNonEmpty
+    New-ParamCompleter -ShortName p -LongName parents -Description $msg.parents
+    New-ParamCompleter -ShortName v -LongName verbose -Description $msg.verbose
+    New-ParamCompleter -LongName help -Description $msg.help
+    New-ParamCompleter -LongName version -Description $msg.version
 )
