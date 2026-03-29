7d0f31d7705f29aa1935d187ff562b7f55a583b1
teramako <teramako@gmail.com>
2026-03-29 15:43:21 +0900

    ♻️ Fix indent

--- NativeCommandCompleter.psm1
+++ NativeCommandCompleter.psm1
@@ -2,19 +2,19 @@
  #  Root Module
  #>
 
- if ([string]::IsNullOrEmpty($env:PS_COMPLETE_PATH))
- {
-     # Set default environment variable: PS_COMPLETE_PATH
-     #      1. <Profile Directory>/completions
-     #      2. <Module Directory>/completions
-     $dirs = (Join-Path -Path ([System.IO.Path]::GetDirectoryName($PROFILE)) -ChildPath completions),
-             (Join-Path -Path $PSScriptRoot -ChildPath completions)
-     $env:PS_COMPLETE_PATH = $dirs -join [System.IO.Path]::PathSeparator;
- }
+if ([string]::IsNullOrEmpty($env:PS_COMPLETE_PATH))
+{
+    # Set default environment variable: PS_COMPLETE_PATH
+    #      1. <Profile Directory>/completions
+    #      2. <Module Directory>/completions
+    $dirs = (Join-Path -Path ([System.IO.Path]::GetDirectoryName($PROFILE)) -ChildPath completions),
+            (Join-Path -Path $PSScriptRoot -ChildPath completions)
+    $env:PS_COMPLETE_PATH = $dirs -join [System.IO.Path]::PathSeparator;
+}
 
- Register-ArgumentCompleter -NativeFallback -ScriptBlock {
-     param($wordToComplete, $commandAst, $cursorPosition)
-     $currentDirectory = Get-Location -PSProvider FileSystem
-     [MT.Comp.NativeCompleter]::Complete($wordToComplete, $commandAst, $cursorPosition, $Host, $currentDirectory)
- }
+Register-ArgumentCompleter -NativeFallback -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    $currentDirectory = Get-Location -PSProvider FileSystem
+    [MT.Comp.NativeCompleter]::Complete($wordToComplete, $commandAst, $cursorPosition, $Host, $currentDirectory)
+}
 
