9bab0d535fa8499c6aa25adf244ac03821d5857a
teramako <teramako@gmail.com>
2026-05-03 19:28:44 +0900

    💥 Change to treat the value an argument name if it is a string
    on `ArgumentsTransformationAttribute`.

--- src/Commands/ArgumentsTransformationAttribute.cs
+++ src/Commands/ArgumentsTransformationAttribute.cs
@@ -21,50 +21,14 @@ public class ArgumentsTransformationAttribute : ArgumentTransformationAttribute
             inputData = pso.BaseObject;
 
         ArgumentCompleterCollection results = [];
-        IArgumentCompleter ac;
         switch (inputData)
         {
             case IList list:
-                List<CompletionValue> bufList = [];
-                bool processingCompletionData = false;
-                bool processingCompleter = false;
-                for (int i = 0; i < list.Count; i++)
+                foreach (var item in list)
                 {
-                    var item = list[i];
-                    switch (item)
-                    {
-                        case null:
-                            break;
-                        case CompletionValue data:
-                            if (processingCompleter)
-                                throw new ArgumentException(COMPLEX_TYPES_EXCEPTION);
-
-                            bufList.Add(data);
-                            processingCompletionData = true;
-                            break;
-                        case string str:
-                            if (processingCompleter)
-                                throw new ArgumentException(COMPLEX_TYPES_EXCEPTION);
-
-                            bufList.Add(CompletionValue.Parse(str));
-                            processingCompletionData = true;
-                            break;
-                        default:
-                            if (processingCompletionData)
-                                throw new ArgumentException(COMPLEX_TYPES_EXCEPTION);
-
-                            results.Add(ConvertToArgumentCompleter(item, $"{i + 1}"));
-                            break;
-                    }
-                }
-                if (bufList.Count > 0)
-                {
-                    ac = new ArgumentCompleterWithCandidates()
-                    {
-                        Name = "arg", 
-                        Candidates = bufList.ToArray()
-                    };
-                    results.Add(ac);
+                    if (item is null)
+                        continue;
+                    results.Add(ConvertToArgumentCompleter(item));
                 }
                 break;
             default:
@@ -73,40 +37,17 @@ public class ArgumentsTransformationAttribute : ArgumentTransformationAttribute
 
         }
 
-        return results.ToArray();
+        return results;
     }
 
-    public static IArgumentCompleter ConvertToArgumentCompleter(object obj, string suffix = "")
+    public static IArgumentCompleter ConvertToArgumentCompleter(object obj)
     {
         switch (obj)
         {
             case IArgumentCompleter ac:
                 return ac;
             case string str:
-                return new ArgumentCompleterWithCandidates()
-                {
-                    Name = $"arg{suffix}",
-                    Candidates = [CompletionValue.Parse(str)]
-                };
-            case ScriptBlock sb:
-                return new ArgumentCompleterWithScript()
-                {
-                    Name = $"arg{suffix}",
-                    Script = sb
-                };
-            case IList list:
-                object[] arr = [..list];
-                return new ArgumentCompleterWithCandidates()
-                {
-                    Name = $"arg{suffix}",
-                    Candidates = arr.Select(obj => obj switch
-                                            {
-                                                CompletionValue data => data,
-                                                string str => CompletionValue.Parse(str),
-                                                _ => CompletionValue.Parse($"{obj}")
-                                            })
-                                    .ToArray()
-                };
+                return new ArgumentCompleterWithType { Name = str };
             case IDictionary dict:
                 if (LanguagePrimitives.TryConvertTo<ArgumentCompleterWithType>(dict, out var acType))
                 {
