26ac3a96ef032f8bbe59bfa1df45328641e406ee
teramako <teramako@gmail.com>
2026-05-19 21:07:40 +0900

    ♻️ Move `ArgumentElementType` to ArgumentElementType.cs

--- src/Tokenizer.cs
+++ src/Tokenizer.cs
@@ -4,30 +4,6 @@ using System.Management.Automation.Language;
 
 namespace Sabamiso;
 
-[Flags]
-public enum ArgumentElementType
-{
-    String = 1 << 0,
-    Number = 1 << 2,
-    Expression = 1 << 3,
-
-    DoubleQuoted = 1 << 4,
-    SingleQuoted = 1 << 5,
-
-    Variable = 1 << 6,
-    Array = 1 << 7,
-    Hashtable = 1 << 8,
-    Nested = 1 << 9,
-
-    StringDoubleQuoted = String | DoubleQuoted,
-    StringSingleQuoted = String | SingleQuoted,
-
-    VariableExpression = Expression | Variable,
-    ArrayExpression = Expression | Array,
-    HashtableExpression = Expression | Hashtable,
-    NestedExpression = Expression | Nested,
-}
-
 public class ArgumentElement
 {
     public ImmutableArray<Token> Tokens { get; }
